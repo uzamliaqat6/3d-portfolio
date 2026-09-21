eb7d9e15708e5ad21cb807f41755dc4369a4761a
M UZAAM LIAQAT <128187067+uzamliaqat6@users.noreply.github.com>
2026-09-21 19:10:59 +0500

    Update portfolio changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,10 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Video editor and AI video creator from Rawalpindi, and founder of Duovia, a digital marketing agency.With 3 years of experience and 100+ clients, I create polished, high-impact videos using Premiere Pro, After Effects, and AI tools.
+          Video editor and AI video creator from Rawalpindi, and founder of Duovia,
+           a digital marketing agency.With 3 years of experience and 100+ clients,
+            I create polished, high-impact videos using Premiere Pro, After Effects
+             and AI tools.
 
 Have a story to tell? Let's make it unforgettable.
         </p>
